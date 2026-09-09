52c32d1f35f252091cd9d653dfca6f19a745a943
shivamopxd <alncshivamop@gmail.com>
2026-09-10 00:01:14 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-export default function RootLayout({ children }) {
+import './globals.css'
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body style={{margin:0}}>{children}</body>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
